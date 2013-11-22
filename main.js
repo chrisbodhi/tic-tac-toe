9f32be622a659b4718e43cfc1e73817c4a774c6f
chrisbodhi <cboette@gmail.com>
2013-11-22 13:47:17 -0600

    Remove console log to notify winner. Add five other cases to check for winner.

--- main.js
+++ main.js
@@ -28,10 +28,15 @@ var checkForWinner = function () {
   if ( spaces[0] === spaces[1] && spaces[1] === spaces[2]
     || spaces[3] === spaces[4] && spaces[4] === spaces[5]
     || spaces[6] === spaces[7] && spaces[7] === spaces[8]
-    // TODO: Check for rest of game winning cases
+    || spaces[0] === spaces[3] && spaces[3] === spaces[6]
+    || spaces[1] === spaces[4] && spaces[4] === spaces[7]
+    || spaces[2] === spaces[5] && spaces[5] === spaces[8]
+    || spaces[0] === spaces[4] && spaces[4] === spaces[8]
+    || spaces[2] === spaces[4] && spaces[4] === spaces[6]
+    // DONE: Check for rest of game winning cases
   )
   {
-    console.log('somebody won');
+    // console.log('somebody won');
     // DONE: Trigger 'game-win' event with the winning player as the event data
     $(document).trigger('game-win', currentPlayer);
   }
